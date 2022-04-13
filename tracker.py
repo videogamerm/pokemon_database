881878597e0d764b821501f4e8b162757e1773aa
videogamerm <masondwithers@gmail.com>
2022-04-13 08:20:47 -0400

    asdf

---
+++ tracker.py
@@ -0,0 +1,13 @@
+i1 =["","",""]
+i2 =["","",""]
+i3 =["","",""]
+i4 =["","",""]
+i5 =["","",""]
+i6 =["","",""]
+i7 =["","",""]
+i8 =["","",""]
+i9 =["","",""]
+i10 =["","",""]
+
+if input("Typre r to read and anything else to add to the inventory.")== "r":
+    io=input("Which inventory do you want to check.(1,2,3,4,5,6,7,8,9,10,")
